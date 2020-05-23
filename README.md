303c30b9ded983fd16ec1c01b6ce79bf924b6e6c
kharioki <antolisah@gmail.com>
2020-05-23 06:40:04 +0300

    update readme

--- README.md
+++ README.md
@@ -9,4 +9,9 @@
   4. Capitalized. Return a string with the first letter of every word capitalized, e.g. capitalizeLetters('i love javascript') === 'I Love Javascript'
   5. Max-characters. Return the character that is most common in a string, e.g. maxCharacter('javascript') == 'a'
   6. FizzBuzz. Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+  7. Longest word. Return the longest word of a string.
+  8. Array chunking. Split an array into chunked arrays of a specific length.
+  9. Flatten array. Take an array of arrays and flatten to a single array.
+  10. Anagram. Return true if anagram and false if not.
+  11. Letter changes. Change every letter of the string to the one that follows it and capitalize the vowels
 ```
--- index.js
+++ index.js
@@ -140,6 +140,43 @@ function fizzBuzz() {
   }
 }
 
+//! CHALLENGE 7: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+function longestWord(sen) {
+  // SOLUTION 1 - Return a single longest word
+  // SOLUTION 2 - Return an array and include multiple words if they have the same length
+  // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
+}
+
+//! CHALLENGE 8: ARRAY CHUNKING
+// Split an array into chunked arrays of a specific length
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
+
+function chunkArray(arr, len) {}
+
+//! CHALLENGE 9: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+
+function flattenArray(arrays) {}
+
+//! CHALLENGE 10: ANAGRAM
+// Return true if anagram and false if not
+// ex. 'elbow' === 'below'
+// ex. 'Dormitory' === 'dirty room##'
+
+function isAnagram(str1, str2) {}
+
+//! CHALLENGE 11: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+
+function letterChanges(str) {}
+
 // Call Function
 const output = fizzBuzz();
 
