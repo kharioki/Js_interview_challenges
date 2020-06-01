8b0e54f79e963b50746646427766132a085d4820
kharioki <antolisah@gmail.com>
2020-06-01 03:04:19 +0300

    Challenge 17 - evens and odds sum

--- index.js
+++ index.js
@@ -387,9 +387,16 @@ function missingLetters(str) {
 // ex.
 // evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
 
-function evenOddSums() {}
+function evenOddSums(arr) {
+  let evenSum = 0;
+  let oddSum = 0;
+
+  arr.forEach(num => (num % 2 === 0 ? (evenSum += num) : (oddSum += num)));
+
+  return [evenSum, oddSum];
+}
 
 // Call Function
-const output = missingLetters('abcdefghjklmno');
+const output = evenOddSums([50, 60, 60, 45, 71]);
 
 console.log(output);
